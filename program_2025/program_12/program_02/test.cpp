7040ff27267d86e01784f879035b236438ef3b74
amazy <fanhengjia2596@outlook.com>
2025-12-03 04:00:02 +0800

    Auto-sync: 2025-12-03 04:00:02

---
+++ program_2025/program_12/program_02/test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using i128 = __int128_t;
+
+
+
+void solve()
+{
+    cout << "sdflsjdsf" << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
